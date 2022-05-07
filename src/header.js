cf98f59988d8a590cb547c52818e3c0ab19809d9
DEEPAK KUMAR MANDAL <nomercyhunters@gmail.com>
2022-05-07 20:43:55 +0530

    final index

--- src/header.js
+++ src/header.js
@@ -2,7 +2,7 @@
 // this is navbar property
 
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { myFunction() };
 
 var main_header = document.getElementById("navbar");
 var sticky = main_header.offsetTop;
@@ -21,34 +21,131 @@ function myFunction() {
 
 // this is js code for side panel
 
+// console.log("dhfjkd",window.screen.width)
+// if(window.screen.width>=1204){
+//   document.getElementById("sidepannel").style.width = "550px"
+// }
+
+// console.log(window.matchMedia('(max-width:1200px)').matches)
+
+function check(){
+
+  if(!window.matchMedia('(max-width:1200px)').matches){
+    console.log(!window.matchMedia('(max-width:1200px)').matches)
+    document.querySelector('#sidepannel').style.display = 'flex'
+  
+    // alert('')
+    document.getElementById("sidepannel").style.width = "74%"
+  }else{
+    document.querySelector('#sidepannel').style.display = 'none'
+    closeNav()
+  }
+}
+
+$(document).ready(function(){
+  $(window).resize(function(){
+    check()
+  });
+});
+
+
+
+
+
+
+
+
 
 function openNav() {
-    document.getElementById("sidepannel").style.width = "550px"
+
+
+
+ 
+   document.getElementById("sidepannel").style.width = "550px"
+   document.getElementById('sidepannel').style.display="block"
+
 }
 
 function closeNav() {
-    document.getElementById("sidepannel").style.width = "0"
+  document.getElementById("sidepannel").style.width = "0px"
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //end here
 
 // this is the js code of  meal plan
 let acc = document.querySelector("#sidebar_bottom").children
-console.log(acc[1],acc[4],acc[6])
-
+// console.log(acc[1], acc[4], acc[6])
 //1
 
-acc[1].addEventListener('click', function(){
-    this.classList.toggle("active");
-    let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none"
-        } else {
-            panel.style.display = "block"
-        }
-})            
+acc[1].addEventListener('click', function () {
+  this.classList.toggle("active");
+  let panel = this.nextElementSibling;
+  let check = window.matchMedia('(max-width:1209px)')
+  if (check.matches) {
 
-// this is the js code of  meal plan end here
 
+
+    if (panel.style.display === "block") {
+      panel.style.display = "none"
+    } else {
+      panel.style.display = "block"
+    }
+  }
+
+})
+// console.log(acc[1])
+acc[1].addEventListener('onmouseover',function(){
+ 
+  this.classList.toggle("active");
+  let panel = this.nextElementSibling;
+  let check = window.matchMedia('(max-width:1209px)')
+  if (!check.matches) {
+    if (panel.style.display === "block") {
+      panel.style.display = "none"
+    } else {
+      panel.style.display = "block"
+    }
+  }
+})
+
+
+// }
 
 
