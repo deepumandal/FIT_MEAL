b5f54198b607e8cde10201f1ded713c293348f03
shrishtiprasad <shrishtiprasad12@gmail.com>
2022-05-08 19:30:01 +0530

    about us page added

---
+++ components/bagComp.js
@@ -0,0 +1,12 @@
+
+function bag(){
+
+    return `
+        <div id="item">0</div>
+        <button>
+            <i class="fa-solid fa-bag-shopping"></i>
+        </button>`
+    ;
+};
+
+export default bag();
